00484865c4649dc6a37d0d183e3a7c5829b56d02
Andreas Dilger <adilger@dilger.ca>
2026-08-12 10:04:06 -0600

    cfs: change banner to remove ISC'26 CFS

--- templates/element/call.php
+++ templates/element/call.php
@@ -1,3 +1,6 @@
 <div class="call">
-    Submit your IO500 results for the upcoming <?php echo $this->Html->link("ISC'26", ['controller' => 'pages', 'action' => 'display', 'bof-isc26']); ?> list, which will be presented at our BOF. See the <?php echo $this->Html->link('Call for Submissions', ['controller' => 'pages', 'action' => 'display', 'cfs-isc26' ]); ?> for details.
+<!--
+Submit your IO500 results for the upcoming <?php echo $this->Html->link("ISC'26", ['controller' => 'pages', 'action' => 'display', 'bof-isc26']); ?> list, which will be presented at our BOF. See the <?php echo $this->Html->link('Call for Submissions', ['controller' => 'pages', 'action' => 'display', 'cfs-isc26' ]); ?> for details.
+-->
+Look for our next call for submissions for the SC'26 lists in late September.
 </div>
